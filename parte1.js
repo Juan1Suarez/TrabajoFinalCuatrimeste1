22004823198582c552ca621475ecf61413086cc9
Juan1Suarez <juansuarez60249@gmail.com>
2023-07-02 00:09:32 -0300

    parte 1 casi completa

---
+++ parte1.js
@@ -0,0 +1,26 @@
+<div class="containercontainer">
+  <div class="position-relative containerform">
+   
+    <form class="form-floating">
+      <input type="email" class="form-control" id="floatingInputValue" placeholder="name@example.com"
+        value="test@example.com">
+      <label for="floatingInputValue">Nombre completo</label>
+      
+      <form class="form-floating">
+        <input type="email" class="form-control" id="floatingInputValue" placeholder="name@example.com"
+          value="test@example.com">
+        <label for="floatingInputValue">Apellido</label>
+      </form>
+
+        <form class="form-floating">
+          <input type="email" class="form-control" id="floatingInputValue" placeholder="name@example.com"
+            value="test@example.com">
+          <label for="floatingInputValue">Email</label>
+        </form>
+
+        <form class="form-floating">
+          <input type="number" class="form-control" id="floatingInputValue" placeholder="name@example.com"
+            value="test@example.com">
+          <label for="floatingInputValue">Numero de teléfono</label>
+        </form>
+  </div>
