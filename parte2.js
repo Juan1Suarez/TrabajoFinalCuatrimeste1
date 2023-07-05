1d888580ffb530c49c3c0945e0018e8eb8823d23
Juan1Suarez <juansuarez60249@gmail.com>
2023-07-05 17:47:57 -0300

    .

--- parte2.js
+++ parte2.js
@@ -1,24 +1,43 @@
-function añadirtarea() {
-    const contenedor = document.getElementById("contenedor");
-    const agregartexto = document.getElementById("tituloTarea")
-    
-    const divtarea = document.createElement ("div");
-    divtarea.className = "tarea"
-    
-    const para = document.createElement("p");
-    para.innerText= "titulotarea.value";
-    
-    divtarea.appendChild(para);
-    contenedor.appendChild(divtarea);
+const input = document.querySelector("input");
+const añadirboton = document.querySelector(".añadirboton");
+const ul = document.querySelector("ul");
+const tareaspe = document.querySelector(".tareaspe");
+
+añadirboton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text !== "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(añadirbotonborrar());
+    ul.appendChild(li);
+
+    input.value = "";
+    tareaspe.style.display = "none";
+  }
+});
+
+function añadirbotonborrar() {
+  const botonborrar = document.createElement("button");
+
+  botonborrar.textContent = "X";
+  botonborrar.className = "botonborrar";
+
+  botonborrar.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+
+    const items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      tareaspe.style.display = "block";
     }
-    
-    const inicializarJs = () => {
-        dibujarTabla();
-      
-        const boton = document.getElementById("agregarTarea");
-        boton.addEventListener('click', function (e) {
-          agregarTareaFija();
-        });
-      }
-      
-      window.addEventListener('load', inicializarJs);
+  });
+
+  return botonborrar;
+}
